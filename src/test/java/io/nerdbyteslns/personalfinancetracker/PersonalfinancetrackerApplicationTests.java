10cbcddf5b4be97dfe946fb9cd114f4a38356fde
Matthew Oduamafu <mattoduamafu@gmail.com>
2024-07-21 00:38:43 +0000

    Initial commit

---
+++ src/test/java/io/nerdbyteslns/personalfinancetracker/PersonalfinancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerdbyteslns.personalfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalfinancetrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
